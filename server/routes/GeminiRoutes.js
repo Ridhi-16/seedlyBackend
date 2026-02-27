cb81c386d935b5177ee32791a53b04b4d42b0a52
Ridhi-16 <ridhi8012@gmail.com>
2026-02-27 13:47:39 +0530

    ai added

--- server/routes/GeminiRoutes.js
+++ server/routes/GeminiRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { suggestCrop } = require("../apis/Gemini/GeminiController");
-
+const { voiceProgress } = require("../controllers/aiController");
 const router = express.Router();
 
 router.post("/suggest", suggestCrop);
+
+
+router.post("/voice-progress", voiceProgress);
 
 module.exports = router;
